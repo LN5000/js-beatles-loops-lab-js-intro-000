7696f10e038051e8fd30a816296c0a92adc63f55
Lee <lee.o.noble@gmail.com>
2018-06-29 06:52:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function theBeatlesPlay(musicians,instruments) {
 
 function johnLennonFacts(facts) {
   var output = [];
+  var i;
   while (i < facts.length) {
     output.push(`facts[i]!!!`)
     i++;
