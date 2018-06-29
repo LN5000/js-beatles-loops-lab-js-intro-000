bef43c79add08e2f0e0baab868c6e727996bce3c
Lee <lee.o.noble@gmail.com>
2018-06-29 06:44:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function theBeatlesPlay(musicians,instruments) {
   var l = musicians.length;
   var sentences = [];
   for (i = 0; i < l; i++) {
-    text += "The number is " + i + "<br>";
+    sentences.push("${musicians[i]} plays ${instruments[i]}");
 }
 }
